e68d77ca497fa88b70865e3dcccefa5a87b59c45
Thymen Janson <s1146329@student.hsleiden.nl>
2025-11-14 10:40:45 +0100

    Initial commit

---
+++ src/main/java/com/example/solveitwebsitebackend/SolveItWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.solveitwebsitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolveItWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolveItWebsiteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/solveitwebsitebackend/SolveItWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solveitwebsitebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolveItWebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
