20f40b6140eac1da42fd0d1dae3720ad0b1e97fe
Thymen Janson <s1146329@student.hsleiden.nl>
2025-11-17 11:53:31 +0100

    Feature: Maps Objectives to a structure for the front-end, made a few changes on DAO for that

--- src/main/java/com/example/solveitwebsitebackend/dao/ObjectiveDAO.java
+++ src/main/java/com/example/solveitwebsitebackend/dao/ObjectiveDAO.java
@@ -1,5 +1,9 @@
 package com.example.solveitwebsitebackend.dao;
 
+import java.util.List;
+
+import com.example.solveitwebsitebackend.mapper.ObjectiveMapper;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Repository;
@@ -11,27 +15,59 @@ public class ObjectiveDAO {
     private final RestTemplate restTemplate = new RestTemplate();
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public JsonNode fetchObjectives(String objectiveGithubUrl) {
+    public List<ObjectiveMapper.RawObjective> fetchRawObjectives(String objectivesGithubUrl) {
         try {
-            String objectivesJson = restTemplate.getForObject(objectiveGithubUrl, String.class);
-
-            return objectMapper.readTree(objectivesJson);
+            String objectivesJson = restTemplate.getForObject(objectivesGithubUrl, String.class);
+            JavaType objectivesType = objectMapper.getTypeFactory().constructCollectionType(List.class, ObjectiveMapper.RawObjective.class);
+            return objectMapper.readValue(objectivesJson, objectivesType);
         } catch (Exception e) {
-            throw new RuntimeException("Failed to fetch or parse Objective json-file");
+            throw new RuntimeException("Failed to fetch or parse Objective json-file", e);
         }
     }
 
-    // --Print data for test--
-    public void printFetchedObjectives(String url) {
-        JsonNode jsonNode = fetchObjectives(url);
-        System.out.println(jsonNode.toPrettyString());
+    public List<ObjectiveMapper.NewObjective> mapFetchedObjectives(String objectivesGithubUrl) {
+        List<ObjectiveMapper.RawObjective> rawObjectiveList = fetchRawObjectives(objectivesGithubUrl);
+        return rawObjectiveList.stream().map(ObjectiveMapper::map).toList();
     }
+
+    public void printMappedObjectives(String url) {
+        try {
+            List<ObjectiveMapper.NewObjective> objectives = mapFetchedObjectives(url);
+            String objectivesJson = objectMapper
+                    .writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(objectives);
+
+            System.out.println(objectivesJson);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to print objectives in Json", e);
+        }
+    }
+
+//    //--Fetch data to print--
+//    public JsonNode fetchObjectives(String objectiveGithubUrl) {
+//        try {
+//            String objectivesJson = restTemplate.getForObject(objectiveGithubUrl, String.class);
+//
+//            return objectMapper.readTree(objectivesJson);
+//        } catch (Exception e) {
+//            throw new RuntimeException("Failed to fetch or parse Objective json-file", e);
+//        }
+//    }
+//
+//    //--Print fetched data--
+//    public void printFetchedObjectives(String url) {
+//        JsonNode jsonNode = fetchObjectives(url);
+//        System.out.println(jsonNode.toPrettyString());
+//    }
 
     public static void main(String[] args) {
         ObjectiveDAO dao = new ObjectiveDAO();
 
-        dao.printFetchedObjectives(
-                "https://raw.githubusercontent.com/SOLVE-IT-DF/solve-it/refs/heads/main/data/solve-it.json"
-        );
+        dao.printMappedObjectives(
+                "https://raw.githubusercontent.com/SOLVE-IT-DF/solve-it/refs/heads/main/data/solve-it.json");
+
+//        dao.printFetchedObjectives(
+//                "https://raw.githubusercontent.com/SOLVE-IT-DF/solve-it/refs/heads/main/data/solve-it.json"
+//        );
     }
 }
