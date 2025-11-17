84df40b38d4f0c39407b47bb883df7604d1c7541
Thymen Janson <s1146329@student.hsleiden.nl>
2025-11-17 15:16:43 +0100

    Feature: Maps Weaknesses to a structure for the front-end, made a few changes on DAO for that

--- src/main/java/com/example/solveitwebsitebackend/dao/WeaknessDAO.java
+++ src/main/java/com/example/solveitwebsitebackend/dao/WeaknessDAO.java
@@ -1,5 +1,7 @@
 package com.example.solveitwebsitebackend.dao;
 
+import com.example.solveitwebsitebackend.mapper.WeaknessMapper;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Repository;
@@ -14,19 +16,21 @@ public class WeaknessDAO {
     private final RestTemplate restTemplate = new RestTemplate();
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public List<JsonNode> fetchWeaknesses(String weaknessGithubUrl) {
+    public List<WeaknessMapper.RawWeakness> fetchRawWeaknesses(String weaknessGithubUrl) {
         try {
             JsonNode filesArray = objectMapper.readTree(restTemplate.getForObject(weaknessGithubUrl, String.class));
 
-            List<JsonNode> weaknesses = new ArrayList<>();
+            List<WeaknessMapper.RawWeakness> weaknesses = new ArrayList<>();
 
             for (JsonNode fileNode : filesArray) {
                 if ("file".equals(fileNode.get("type").asText()) && fileNode.get("name").asText().endsWith(".json")) {
                     String singleUrl = fileNode.get("download_url").asText();
-                    String jsonContent = restTemplate.getForObject(singleUrl, String.class);
 
-                    JsonNode weaknessNode = objectMapper.readTree(jsonContent);
-                    weaknesses.add(weaknessNode);
+                    String weaknessJson = restTemplate.getForObject(singleUrl, String.class);
+                    JavaType weaknessType = objectMapper.getTypeFactory().constructType(WeaknessMapper.RawWeakness.class);
+
+                    WeaknessMapper.RawWeakness rawWeakness = objectMapper.readValue(weaknessJson, weaknessType);
+                    weaknesses.add(rawWeakness);
                 }
             }
 
@@ -36,18 +40,64 @@ public class WeaknessDAO {
         }
     }
 
-    // --Print data for test--
-    public void printFetchedWeaknesses(String url) {
-        List<JsonNode> jsonNodes = fetchWeaknesses(url);
-        for (JsonNode jsonNode : jsonNodes) {
-            System.out.println(jsonNode.toPrettyString());
-        }
+    public List<WeaknessMapper.NewWeakness> mapFetchedWeaknesses(String weaknessGithubUrl) {
+        List<WeaknessMapper.RawWeakness> rawWeaknesses = fetchRawWeaknesses(weaknessGithubUrl);
+        return rawWeaknesses.stream().map(WeaknessMapper::map).toList();
     }
+
+    public void printMappedWeaknesses(String url) {
+        try {
+            List<WeaknessMapper.NewWeakness> weaknesses = mapFetchedWeaknesses(url);
+            String weaknessJson = objectMapper
+                    .writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(weaknesses);
+
+            System.out.println(weaknessJson);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to print weaknesses in Json", e);
+        }
+
+    }
+
+//    //--Fetch data to print--
+//    public List<JsonNode> fetchWeaknesses(String weaknessGithubUrl) {
+//        try {
+//            JsonNode filesArray = objectMapper.readTree(restTemplate.getForObject(weaknessGithubUrl, String.class));
+//
+//            List<JsonNode> weaknesses = new ArrayList<>();
+//
+//            for (JsonNode fileNode : filesArray) {
+//                if ("file".equals(fileNode.get("type").asText()) && fileNode.get("name").asText().endsWith(".json")) {
+//                    String singleUrl = fileNode.get("download_url").asText();
+//                    String jsonContent = restTemplate.getForObject(singleUrl, String.class);
+//
+//                    JsonNode weaknessNode = objectMapper.readTree(jsonContent);
+//                    weaknesses.add(weaknessNode);
+//                }
+//            }
+//
+//            return weaknesses;
+//        } catch (Exception e) {
+//            throw new RuntimeException("Failed to fetch weaknesses dynamically");
+//        }
+//    }
+//
+//    //--Print fetched data--
+//    public void printFetchedWeaknesses(String url) {
+//        List<JsonNode> jsonNodes = fetchWeaknesses(url);
+//        for (JsonNode jsonNode : jsonNodes) {
+//            System.out.println(jsonNode.toPrettyString());
+//        }
+//    }
 
     public static void main(String[] args) {
         WeaknessDAO dao = new WeaknessDAO();
-        dao.printFetchedWeaknesses(
+
+        dao.printMappedWeaknesses(
                 "https://api.github.com/repos/SOLVE-IT-DF/solve-it/contents/data/weaknesses/"
         );
+//        dao.printFetchedWeaknesses(
+//                "https://api.github.com/repos/SOLVE-IT-DF/solve-it/contents/data/weaknesses/"
+//        );
     }
 }
