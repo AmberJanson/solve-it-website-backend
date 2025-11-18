f738f55a5818d2d0ef67e7a96d12c95238fdee9f
Thymen Janson <s1146329@student.hsleiden.nl>
2025-11-18 13:39:03 +0100

    Feature: TechniqueController is made to call all data or single entries (tested with Postman)

---
+++ src/main/java/com/example/solveitwebsitebackend/controller/TechniqueController.java
@@ -0,0 +1,30 @@
+package com.example.solveitwebsitebackend.controller;
+
+import com.example.solveitwebsitebackend.mapper.TechniqueMapper;
+import com.example.solveitwebsitebackend.service.TechniqueService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/techniques")
+public class TechniqueController {
+
+    private final TechniqueService service;
+
+    public TechniqueController(TechniqueService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<Map<String, TechniqueMapper.NewTechnique>> getAllTechniques() {
+        return ResponseEntity.ok(service.getAllTechniques());
+    }
+
+    @GetMapping(params = "techniqueId")
+    public ResponseEntity<TechniqueMapper.NewTechnique> getTechniqueById(@RequestParam String techniqueId) {
+        return ResponseEntity.ok(service.getTechniqueById(techniqueId));
+    }
+}
